2376a9414241c23c302f7c0b0e12fdd3223774a1
easty <white@easty.ru>
2026-02-02 22:10:30 +0300

    rewrite keyboard

--- speccy-hardware/src/main/java/spectrum/hardware/input/KeyboardImpl.java
+++ speccy-hardware/src/main/java/spectrum/hardware/input/KeyboardImpl.java
@@ -3,6 +3,17 @@ package spectrum.hardware.input;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Keyboard matrix:
+ * * Bit 0: SHIFT, Z, X, C, V
+ * * Bit 1: A, S, D, F, G
+ * * Bit 2: Q, W, E, R, T
+ * * Bit 3: 1, 2, 3, 4, 5
+ * * Bit 4: 0, 9, 8, 7, 6
+ * * Bit 5: P, O, I, U, Y
+ * * Bit 6: ENTER, L, K, J, H
+ * * Bit 7: SPACE, SYMBOL, M, N, B
+ */
 public class KeyboardImpl implements Keyboard {
 
     @Getter
@@ -11,12 +22,16 @@ public class KeyboardImpl implements Keyboard {
 
     @Override
     public void init() {
-
+        if (keyboardDriver != null) {
+            keyboardDriver.init();
+        }
     }
 
     @Override
     public void reset() {
-
+        if (keyboardDriver != null) {
+            keyboardDriver.reset();
+        }
     }
 
     @Override
@@ -26,7 +41,7 @@ public class KeyboardImpl implements Keyboard {
 
     @Override
     public void close() {
-
+        // do nothing
     }
 
     @Override
